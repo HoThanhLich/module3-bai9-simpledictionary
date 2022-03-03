2b4cf5db40c6a2843ea006f2489cb6656da35bf5
HoThanhLich <thanhlichpvmtc@gmail.com>
2022-03-03 15:15:16 +0700

    them file

---
+++ src/main/java/SimpleDictionaryServlet.java
@@ -0,0 +1,38 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "SimpleDictionaryServlet", value = "/translate")
+public class SimpleDictionaryServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello","Xin chào");
+        dictionary.put("how","thế nào");
+        dictionary.put("book","quyển vở");
+        dictionary.put("computer","máy tính");
+
+        String search = request.getParameter("txtSearch");
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+
+        String result = dictionary.get(search);
+        if (result != null) {
+            printWriter.println("word: " + search);
+            printWriter.println("Result: " + result);
+        } else {
+            printWriter.println("Not found");
+        }
+        printWriter.println("</html>");
+    }
+}
